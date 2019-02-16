c4036cf0740c9a69fd65ecf1a07db446149b9ac6
exctues <alexnamecode@gmail.com>
2019-02-16 10:20:15 +0300

    Lab3

---
+++ Lab3/common.h
@@ -0,0 +1,13 @@
+typedef struct _test_struct{
+    
+    char name[32];
+    unsigned int age;
+    unsigned int group;
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+    unsigned int c;
+
+} result_struct_t;
